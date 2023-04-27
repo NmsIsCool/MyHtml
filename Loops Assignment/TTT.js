4b5ffc77c30940ec90c960728643208b4a2e013c
NMSIsCool <jackaroosherwood@gmail.com>
2023-04-27 12:33:39 -0400

    It's finished just doesnt work

--- Loops Assignment/TTT.js
+++ Loops Assignment/TTT.js
@@ -23,6 +23,45 @@ function go(){
     var content=$(this).html();
 
     if(content==="-"){
-        
+         $(this).html(turn);
+         if(turn=="X"){
+            turn="O";}else {turn="X";}
+         }
+        }
+function restart(){
+    for(var x=1; x<=9; x++){
+        $("#btn"+x).html("-");
+        $("#btn"+x).css("background", "#DDD");
     }
 }
+
+function checkwin(a,b,c){
+    var box1 =$("#btn"+a).html();
+    var box2 =$("#btn"+b).html();
+    var box3 =$("#btn"+c).html();
+
+    if(box1+box2+box3=="XXX"){
+        $("#btn"+a).css("background", "red");
+        $("#btn"+b).css("background", "red");
+        $("#btn"+c).css("background", "red");
+        alert("X wins - click any square to start a new game");
+        gameover=true;
+    }
+    if(box1+box2+box3=="OOO"){
+        $("#btn"+a).css("background", "red");
+        $("#btn"+b).css("background", "red");
+        $("#btn"+c).css("background", "red");
+        alert("O wins - click any square to start a new game");
+        gameover=true;
+    }
+    
+}
+checkwin(1,2,3);
+checkwin(4,5,6);
+checkwin(7,8,9);
+checkwin(1,4,7);
+checkwin(2,5,8);
+checkwin(3,6,9);
+checkwin(1,5,9);
+checkwin(3,5,7);
+    
