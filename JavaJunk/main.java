79d6266d71075a7a1f425498edc3ae4457ceeedd
NMSIsCool <jackaroosherwood@gmail.com>
2023-05-06 20:39:12 -0400

    Experimenting With Java in VSC

---
+++ JavaJunk/main.java
@@ -0,0 +1,11 @@
+import java.awt.Font;
+
+public class main {
+    public static void out(String str) {
+        System.out.println(str);
+    }
+
+    public static void main(String args[]) {
+        out("I like men");
+    }
+}
